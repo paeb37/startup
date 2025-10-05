dfc19587dfe5208fcf358e2fee4d9f4223d0670f
paeb37 <btp2109@columbia.edu>
2025-10-05 13:10:44 -0400

    removed test

--- test/view_pptx.py
+++
@@ -1,9 +0,0 @@
-import json, base64
-
-with open('love.json') as f:
-    data = json.load(f)
-
-with open('love.pptx', 'wb') as f:
-    f.write(base64.b64decode(data['export']['pptxBase64']))
-
-print('PPTX saved')
